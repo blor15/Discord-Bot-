a62653571a876e4b548256d5d4ddf9c2ca938b28
Brandon <blor15@hotmail.com>
2022-04-23 17:02:08 -0600

    Welcome message for users when they join the server

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import Discord, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import { generateImage } from './generateimage.js';
+
 const client = new Discord.Client({
     intents: [
         'GUILDS',
-        'GUILD_MESSAGES'
+        'GUILD_MESSAGES',
+        'GUILD_MEMBERS'
     ]
 });
 
@@ -17,6 +20,17 @@ client.on('messageCreate', (message) => {
     if (message.content === 'Hi') {
         message.reply('Hello World')
     }
+})
+
+const weclomeChannelId = "862519578671185941";
+
+client.on("guildMemberAdd", async (member) => {
+    const image = await generateImage(member)
+    member.guild.channels.cache.get(weclomeChannelId).send({
+        content: (`<@${member.id}> Welcome to the server!`),
+        files: [image]
+    })
+
 })
 
 client.login(process.env.TOKEN);
